080a2b5b9387b6111a28d1e6ff08f9ac1f3ece09
Idan Fishman <idanfishmanswe@gmail.com>
2023-01-12 15:35:40 +0200

    style

--- blue-pixel-fastapi/main.py
+++ blue-pixel-fastapi/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
 @app.get("/")
 def read_color():
     return {"color": "blue", "light_hexa": "#2684FC", "dark_hexa": "#0066DA"}
--- green-pixel-fastapi/ main.py
+++ green-pixel-fastapi/ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
 @app.get("/")
 def read_color():
     return {"color": "green", "light_hexa": "#00AC47", "dark_hexa": "#00832D"}
